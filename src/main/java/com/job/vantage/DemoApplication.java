15591b0fe8b6fe47d87f442c3b99b1615f83b6e7
Harsh Rawal <rawalharsh48@gmail.com>
2020-02-10 14:04:22 -0500

    Final commit

---
+++ src/main/java/com/job/vantage/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.job.vantage;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		GetData.Start_Collection();
+	}
+
+}
---
+++ src/main/java/com/job/vantage/Urls_and_components.java
@@ -0,0 +1,106 @@
+package com.job.vantage;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jsoup.nodes.Element;
+
+public class Urls_and_components {
+    private String BaseUrl ;
+    private String topics_list;
+    private String FileName;
+    private String PageList;
+    private String url_slicer;
+    private String reviews_list;
+    private String library_url;
+    private String authors;
+    private String date;
+    private List<String> months;
+    private Map<String, String> map;
+
+    public Urls_and_components(){
+        BaseUrl = "https://www.cochranelibrary.com/cdsr/reviews/topics"; 
+        topics_list = "li[class=browse-by-list-item]";
+        FileName = "cochrane-reviews.txt";
+        PageList = "ul[class=pagination-page-list]";
+        url_slicer = "&p_p_id=";
+        reviews_list = "div[class=search-results-item-body]";
+        library_url = "http://onlinelibrary.wiley.com";
+        authors = "div[class=search-result-authors]";
+        date = "div[class=search-result-date]";
+        months = Arrays.asList(
+            "january",
+            "february",
+            "march",
+            "april",
+            "may",
+            "june",
+            "july",
+            "august",
+            "september",
+            "october",
+            "november",
+            "december");
+        map = new HashMap<>();
+
+        for(int i = 0 ;i < months.size(); i++){
+            map.put(months.get(i) , ( (i+1)+"") );
+        }
+
+
+    }
+
+    public String GetBaseUrl(){
+        return BaseUrl;
+    }
+    public String GetTopics_list(){
+        return topics_list;
+    }
+
+    public String GetFileName(){
+        return FileName;
+    }
+
+    public String GetPageList(){
+        return PageList;
+    }
+
+    public String GetTitle(Element topic){
+        return topic.select("a").first().text();
+    }
+
+    public String GetPageUrl(Element page){
+        String page_url_comp = page.attr("href");
+        String page_url_slice = page_url_comp.substring(page_url_comp.indexOf(url_slicer));
+        return page_url_slice;
+    }
+
+
+    public String GetReviews(){
+        return reviews_list;
+    }
+
+    public String GetUrl(Element element){
+        return library_url + element.select("a").attr("href").substring(5);
+    }
+
+    public String GetResults(Element element ,String component){
+        return element.select(component).first().text();
+    }
+    public String author_component(){
+        return authors;
+    }
+
+    public String Date(){
+        return date;
+    }
+    public String MapDate(String component){
+        String[] temp_date = component.split(" ");
+        temp_date[1] = map.get(temp_date[1].toLowerCase());
+        return temp_date[2] + "-" + temp_date[1] + "-" + temp_date[0];
+    }
+    
+
+}
